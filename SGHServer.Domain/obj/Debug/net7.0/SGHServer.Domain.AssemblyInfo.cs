c35e99e0cb7971fcb13624509e0a2dbf34f86cf6
Егор Солоницын <e.solonicyn>
2023-12-07 13:53:31 +0300

    SGH-2 * fast fix

--- SGHServer.Domain/obj/Debug/net7.0/SGHServer.Domain.AssemblyInfo.cs
+++ SGHServer.Domain/obj/Debug/net7.0/SGHServer.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGHServer.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60dd2e6d4d1c2cd4fd8820eaa727157c0c777202")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGHServer.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGHServer.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
