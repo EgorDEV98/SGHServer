f579627609ab5435224406678fbb7a22db2c0d17
Егор Солоницын <e.solonicyn>
2023-12-20 18:12:46 +0300

    SGH-9 * Добавил Query для девайса

---
+++ SGHServer.API/RequestModel/Device/GetDeviceModel.cs
@@ -0,0 +1,6 @@
+﻿namespace SGHServer.API.RequestModel;
+
+public class GetDeviceModel
+{
+    public Guid DeviceUid { get; set; }
+}
---
+++ SGHServer.Application/Repository/DeviceRepository/Query/GetDeviceQuery/GetDeviceQueryCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SGHServer.Application.Exceptions;
+using SGHServer.Application.Interfaces;
+using SGHServer.Application.Mapping;
+using SGHServer.Application.Response.VMs;
+
+namespace SGHServer.Application.Repository.DeviceRepository.Query.GetDeviceQuery;
+
+public class GetDeviceQueryCommandHandler : IRequestHandler<GetDeviceQueryCommand, DeviceVM>
+{
+    private readonly IDataStore _dataStore;
+
+    public GetDeviceQueryCommandHandler(IDataStore dataStore)
+    {
+        _dataStore = dataStore;
+    }
+    
+    public async Task<DeviceVM> Handle(GetDeviceQueryCommand request, CancellationToken cancellationToken)
+    {
+        var device = await _dataStore.Devices
+            .FirstOrDefaultAsync(x => x.DeviceUid == request.DeviceUid, cancellationToken);
+
+        if (device == null)
+        {
+            throw new NotFoundException("Устройство не найдено!");
+        }
+
+        return device.Map();
+    }
+}
